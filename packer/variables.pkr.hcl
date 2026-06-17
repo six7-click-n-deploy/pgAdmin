4926b8b4bdae1c80ac7bab87711917efdf75e360
Leon Priemer <leon.priemer@gmail.com>
2026-06-17 22:38:12 +0200

    Update: Variablenbeschreibung [CONTRACTS] wieder beschriftet inklusive Backend Notation @...

--- packer/variables.pkr.hcl
+++ packer/variables.pkr.hcl
@@ -1,17 +1,17 @@
 variable "image_name" {
   type        = string
-  description = "@openstack:image:name"
+  description = "[PLATFORM] Name des zu erstellenden Images (z.B. online-ide-v1) @openstack:image:name"
   default     = "pgadmin-vX"
 }
 
 variable "networks" {
   type        = list(string)
-  description = "@openstack:network:id:list"
+  description = "[PLATFORM] Netzwerk-UUIDs für Build-VM (von Platform-Admin konfiguriert) @openstack:network:id:list"
   default     = ["4971e080-966d-485e-a161-3e2b7fefad53"]
 }
 
 variable "security_groups" {
   type        = list(string)
-  description = "@openstack:security_group:id:list"
+  description = "[PLATFORM] Security Groups für Build-VM (von Platform-Admin konfiguriert) @openstack:security_group:id:list"
   default     = ["4ffaf007-df66-4250-9118-1bd99378d34a"]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,8 @@
+########################################
+# CUSTOM-Variablen (Optional)
+# Werden vom User gesetzt
+########################################
+
 variable "users" {
   description = "[CONTRACT] Teams mit User-Emails"
   type = map(list(object({
@@ -6,26 +11,31 @@ variable "users" {
   default = {}
 }
 
+########################################
+# CONTRACT-Variablen (PFLICHT)
+# Werden vom Worker/Platform gesetzt
+########################################
+
 variable "image_name" {
-  description = "@openstack:image:name"
+  description = "[BACKEND] Name des Packer-Images aus Glance (z.B. online-ide-v1) @openstack:image:name"
   type        = string
   default     = "pgadmin-vX"
 }
 
 variable "network_uuid" {
-  description = "@openstack:network:id"
+  description = "[BACKEND] UUID des internen Netzwerks (von Platform-Admin konfiguriert) @openstack:network:id"
   type        = string
   default     = "34a00b87-57ce-42c4-8e1b-9ea8a657ec2e"
 }
 
 variable "floating_ip_pool" {
-  description = "@openstack:floating_ip_pool:name"
+  description = "[BACKEND] Name des External Networks für Floating IPs (von Platform-Admin konfiguriert) @openstack:floating_ip_pool:name"
   type        = string
   default     = "DHBW"
 }
 
 variable "shared_secgroup_id" {
-  description = "@openstack:security_group:id"
+  description = "[BACKEND] ID der gemeinsamen Security Group für alle VMs @openstack:security_group:id"
   type        = string
   default     = "4ffaf007-df66-4250-9118-1bd99378d34a"
 }
