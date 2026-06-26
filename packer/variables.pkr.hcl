04df6353dfd4f7d15919b717179de721a2784cdf
Leon Priemer <leon.priemer@gmail.com>
2026-06-26 23:09:16 +0200

    Update der App als Anpassung für die Änderungen am Deploymentprozess

--- packer/variables.pkr.hcl
+++ packer/variables.pkr.hcl
@@ -1,17 +1,16 @@
 variable "image_name" {
   type        = string
-  description = "[PLATFORM] Name des zu erstellenden Images (z.B. online-ide-v1) @openstack:image:name"
-  default     = "pgadmin-vX"
+  description = "Glance-Image-Name — vom Worker zur Build-Zeit gesetzt. @platform:internal"
 }
 
 variable "networks" {
   type        = list(string)
-  description = "[PLATFORM] Netzwerk-UUIDs für Build-VM (von Platform-Admin konfiguriert) @openstack:network:id:list"
+  description = "@openstack:network:id:list Build-Netzwerke"
   default     = ["4971e080-966d-485e-a161-3e2b7fefad53"]
 }
 
 variable "security_groups" {
   type        = list(string)
-  description = "[PLATFORM] Security Groups für Build-VM (von Platform-Admin konfiguriert) @openstack:security_group:id:list"
+  description = "@openstack:security_group:id:list Build-Security-Groups"
   default     = ["4ffaf007-df66-4250-9118-1bd99378d34a"]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,41 +1,38 @@
 ########################################
-# CUSTOM-Variablen (Optional)
-# Werden vom User gesetzt
+# Pflicht-Variablen (vom Worker injiziert)
 ########################################
 
 variable "users" {
-  description = "[CONTRACT] Teams mit User-Emails"
+  description = "Per-team roster — vom Worker injiziert. @platform:internal"
   type = map(list(object({
     email = string
   })))
   default = {}
 }
 
-########################################
-# CONTRACT-Variablen (PFLICHT)
-# Werden vom Worker/Platform gesetzt
-########################################
-
 variable "image_name" {
-  description = "[BACKEND] Name des Packer-Images aus Glance (z.B. online-ide-v1) @openstack:image:name"
+  description = "Glance-Image-Name — vom Worker zur Apply-Zeit gesetzt. @platform:internal"
   type        = string
-  default     = "pgadmin-vX"
 }
 
+########################################
+# Konfigurierbare Variablen (vom Deployer gesetzt)
+########################################
+
 variable "network_uuid" {
-  description = "[BACKEND] UUID des internen Netzwerks (von Platform-Admin konfiguriert) @openstack:network:id"
+  description = "UUID des internen Netzwerks @openstack:network:id"
   type        = string
   default     = "34a00b87-57ce-42c4-8e1b-9ea8a657ec2e"
 }
 
 variable "floating_ip_pool" {
-  description = "[BACKEND] Name des External Networks für Floating IPs (von Platform-Admin konfiguriert) @openstack:floating_ip_pool:name"
+  description = "Name des External Networks für Floating IPs @openstack:floating_ip_pool:name"
   type        = string
   default     = "DHBW"
 }
 
 variable "shared_secgroup_id" {
-  description = "[BACKEND] ID der gemeinsamen Security Group für alle VMs @openstack:security_group:id"
+  description = "ID der gemeinsamen Security Group für alle VMs @openstack:security_group:id"
   type        = string
   default     = "4ffaf007-df66-4250-9118-1bd99378d34a"
 }
